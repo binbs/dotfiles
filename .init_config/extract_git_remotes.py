606db3dcd463e3a972a3dbe809e4bdb33b25287a
binbsoffn <9432238+binbsoffn@users.noreply.github.com>
2020-07-05 22:16:50 +0200

    added some mechs for loading and unloading

--- .init_config/extract_git_remotes.py
+++ .init_config/extract_git_remotes.py
@@ -1,14 +1,33 @@
 #!/usr/bin/python3
 import os
+import argparse
+import pathlib
 
-rootdir = os.getcwd()
-remotes = []
-for directory in os.listdir(rootdir):
-    if os.path.isdir(directory):
-        remotes.append(os.popen(f"git -C {directory} remote -v").read().split()[1])
+parser = argparse.ArgumentParser()
+parser.add_argument("pluginspath", help="path to vim plugins")
+parser.add_argument("-c", "--clone", help="specify whether to save or clone",
+                    action="store_true")
+args = parser.parse_args()
+pluginspath = pathlib.Path(args.pluginspath)
 
-with open("remotes.txt", "w") as f:
-    for line in remotes:
-        f.write(str(line))
-        f.write("\n")
-        print(line)
+path_name = pathlib.PurePath(args.pluginspath).name
+if args.clone:
+    print("cloneing")
+    with open(f"remotes_{path_name}.txt", "r") as f:
+        remotes = f.readlines()
+    for remote in remotes:
+        if not pluginspath.exists():
+            pluginspath.mkdir()
+
+        os.popen(f"git -C {pluginspath} clone {remote}")
+else:
+    remotes = []
+    for entry in os.scandir(args.pluginspath):
+        if entry.is_dir():
+            remotes.append(os.popen(f"git -C {entry.path} remote -v").read().split()[1])
+
+    with open(f"remotes_{path_name}.txt", "w") as f:
+        for line in remotes:
+            f.write(str(line))
+            f.write("\n")
+            print(line)
