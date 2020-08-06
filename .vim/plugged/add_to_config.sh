43d9c23c70d888f950bf95b1011b399fcbb551ff
binbsoffn <9432238+binbsoffn@users.noreply.github.com>
2020-08-06 11:48:31 +0100

    added fzf, pathogen, plug and the adder scripts for submodules

---
+++ .vim/plugged/add_to_config.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cfg_cmd="/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME"
+for extension_name in */; do
+    extension_remote=($(git -C $extension_name remote -v))
+    extension_remote=${extension_remote[1]}
+    CMD="$cfg_cmd submodule add -f $extension_remote $extension_name"
+    output="$(eval $CMD)"
+    echo "$CMD"
+    echo "$output"
+done
+
